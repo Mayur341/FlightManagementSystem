f7809eb190e5559d6b49b5c6d8ca272cec82ac77
Dávid Giczi <40804541+davidgiczi@users.noreply.github.com>
2021-03-08 15:51:39 +0100

    Add files via upload

--- flight/src/main/java/com/giczi/david/flight/repository/PassengerRepository.java
+++ flight/src/main/java/com/giczi/david/flight/repository/PassengerRepository.java
@@ -1,6 +1,7 @@
 package com.giczi.david.flight.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -15,4 +16,9 @@ public interface PassengerRepository extends CrudRepository<Passenger, Long> {
 	List<Passenger> findAll();
 	@Query(value = "select * from passengers where user_name = :user and password = :pass", nativeQuery = true)
 	Passenger findPassengerByUsernameAndPassword(@Param("user") String user, @Param("pass") String pass);
+	@SuppressWarnings("unchecked")
+	Passenger save(Passenger passenger);
+	Optional<Passenger> findById(Long id);
+	
+	
 }
