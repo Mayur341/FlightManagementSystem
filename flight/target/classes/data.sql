bdae10224ea34f85653de36941c6d925f2dc018e
Dávid Giczi <40804541+davidgiczi@users.noreply.github.com>
2021-04-17 15:53:08 +0200

    Delete flight/target directory

--- flight/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO passengers (first_name, last_name, date_of_birth, user_name, password, enabled) VALUES ('Dávid', 'Giczi', '1978-07-12', 'admin', 'YWRtaW4=', true);
-INSERT INTO roles (id, role) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO passengers_roles (passenger_id, role_id) VALUES (1,1);
